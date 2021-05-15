454c4d0ea937ca631d09a3f3a54530e3cef250a6
Praneet Rane <praneet.rane@gmail.com>
2021-05-15 11:05:48 +0530

    Todo App phase 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import react from 'react';
 import reactDOM from 'react-dom';
-
-reactDOM.render(<h1>Good Morning! Praneet</h1>, document.getElementById('root'));
+import App from './App';
+reactDOM.render(<App/>, document.getElementById('root'));
